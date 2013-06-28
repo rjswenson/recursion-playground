2b64c7058a9ad4775d65c5d399dfed81d3652c2b
Robin Swenson-Healey <rjswenson@gmail.com>
2013-06-28 14:19:30 -0700

    perfect v0.2a comments

--- perf_num.rb
+++ perf_num.rb
@@ -2,29 +2,29 @@
 num = 3
 diviser = 2
 
-prime = 0
-def_prime = num%diviser
+prime = 0  
+def_prime = num%diviser  
 
 loop do
     
 	
-	if def_prime > 0 && num>diviser
+	if def_prime > 0 && num>diviser  
 		diviser +=1
-		num = prime
+		num = prime #to add num as prime candidate
 		
 
-	elsif diviser == num-1 && prime ==num
+	elsif diviser == num-1 && prime ==num 
 
 		puts "#{prime}"
 	
 
-	else
+	else            #if num%diviser is 0, def_prime isn't prime
 		num +=1
 		div = 2
-		prime = 0
+		prime = 0	#reset prime value
 	
 	end
-break if num > 100
+break if num > 100	#arbitrary until I figure how to loop correctly
 end
 
 
@@ -32,10 +32,10 @@ end
 
 =begin
 
-pri_seg = (2**prime)-1   #must also be prime
-true_seg = 0			#placeholder to put pri seg w/in
+pri_seg = (2**prime)-10 	#must also be prime
+true_seg = 0				#placeholder to put pri seg w/in
 
-perfect_num = 2**(prime-1)((pri_seg)
+perfect_num = 2**(prime-1)(pri_seg)
 
 if pri_seg%diviser > 0 && pri_seg > deviser -1
 	deviser+=1
