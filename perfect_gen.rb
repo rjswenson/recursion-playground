cb7ffe8d59e4fa9491185606c42677264ff731a2
Robin Swenson-Healey <rjswenson@gmail.com>
2013-06-28 15:23:12 -0700

    rework v0.2

--- perfect_gen.rb
+++ perfect_gen.rb
@@ -1,25 +1,25 @@
 
 
-#Erin approach
-#a perfect number 
-divisor = 2
+#Perfect number finder
+
+divisor = 1
 answer = 0
 sum = 0 
 
 puts "Welcome to the perfect number finder!"
 puts "Specify the range which you would like to search in."
-puts "Your first number?"
-num = gets.chomp
+puts "Your first number? (MUST be greater than 2)"
+num = gets.to_i
 
 puts "And your second?"
-fin_num = gets.chomp
+fin_num = gets.to_i
 
 divs = Array.new
 num_array = Array.new
 
 
 if divisor < num
-	num/divisor = answer
+	num/divisor = answer  #error here, says cannot devise by 0..
 	if num%divisor == 0
 		divs.push answer
 		divisor +=1
