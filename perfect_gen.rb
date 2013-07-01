e84f2e5568ac3eca3fffcaee2000186a263e2271
Robin Swenson-Healey <rjswenson@gmail.com>
2013-07-01 12:11:09 -0700

    perfect v1.0

--- perfect_gen.rb
+++ perfect_gen.rb
@@ -2,14 +2,6 @@
 
 #Perfect number finder
 
-
-#input a number, then divide by a divisor.  If number is integer, then
-#save that number to an array
-
-#divisor = 1
-#factor = 0
-#sum = 0 
-
 puts "Welcome to the perfect number finder!"
 puts "Specify the range which you would like to search in."
 puts "Your first number? (MUST be greater than 2)"
@@ -19,65 +11,38 @@ puts "And your second?"
 input_last= gets.to_i
 
 factors = Array.new
-#num_array = Array.new
+all_perfects = Array.new
 
-sum=0
-for n in 1..input_first
-	if input_first%n ==0
-		factors.push(n)
-	end
-end
+sum = 0
+for x in input_first..input_last
+ 
 
-for n in 0..factors.size-1
-	sum = sum + factors[n]
-end
-
-if input_first == (sum/2)
-	puts "A perfect number: #{input_first}"
-end
-
-
-
-
-
-
-
-
-
-=begin
-
-if divisor < input_first
-	factor = divisor  #error here, says cannot devise by 0..
-	if input_first%divisor == 0
-		factors.push factor
-		divisor +=1
-	else 
-		divisor +=1
-	end
-
-=end
-
-
-
-
-=begin
-elsif divisor == input_first-1
-	divs.inject {|sum,x| sum + x}
-		if divs == input_first
-			num_array.push input_first
-			divisor = 2
-			input_first+=1
-		else
-			divisor = 2
-			input_first +=1
+	for n in 1..x
+		if x%n ==0
+			factors.push(n)
 		end
-elsif input_first == input_last
-	puts "#{num_array}"
+	end
 
-else
-	puts "failed."
+	for n in 0..factors.size-1
+		sum = sum + factors[n]
+	end
+
+	if 	x == (sum/2)
+		puts "A perfect number: #{x}"
+		all_perfects.push(x)
+		sum=0
+		factors.clear
+
+	else
+		sum = 0
+		factors.clear
+	end
+
 end
 
-=end
+
+puts "These are the perfect numbers within your range."
+puts all_perfects.inspect
+
 
 
