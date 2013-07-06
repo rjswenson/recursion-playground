50fb4b3196a4642bbb2c8cbea2ecd1961635e863
Robin Swenson-Healey <rjswenson@gmail.com>
2013-07-06 11:25:36 -0700

    perfect method

--- num_method.rb
+++ num_method.rb
@@ -1,40 +1,45 @@
 
 def get_factors(num1)           #factors here
-  #factors = [] 
+  factors = [] 
   for n in 1..num1
     if num1%n == 0
       factors.push(n)
     end
   end
   p factors.inspect
+  return factors
 end
 
-def sum_of_array(array)
-  sum = 0
+ def sum_of_array(array)
+  $sum = 0
   for n in 0..array.size-1
-    sum = sum +array[n]
+    $sum = $sum +array[n]
   end
-p sum
+return $sum
 end
 
 def perfect_check(num1)
-  if num1 == (sum/2)
+  if num1 == ($sum/2)
     puts "#{num1} is a perfect number."
-    sum = 0
-    factors.clear
+    $sum = 0
+    $factors.clear
   else
-    sum = 0
-    factors.clear
+    $sum = 0
+    $factors.clear
   end
 end
 
 
+p "Let's find some perfect numbers."
+p "First, give me the low number in a range."
+input_low = gets.chomp.to_i
+p "Next, give a high number for the range."
+input_hight = gets.chomp.to_i
 
 
-
-p "What number?"
-input = gets.chomp.to_i
-get_factors(input)
-#sum_of_array(factors) 
-perfect_check(input)
+for x in input_low..input high
+  get_factors(x)
+  sum_of_array($factors)
+  perfect_check(x)
+end
 
