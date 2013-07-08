f8bf1b4f96fde78a12a2ee0f2cd6bdc20e0a57ce
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2013-07-07 20:35:27 -0700

    adding palindrome problem

---
+++ palindrome_integer.rb
@@ -0,0 +1,27 @@
+#######################
+### Palindrome integer
+###
+#
+# Write the methods with the following headers
+#
+# def reverse_number(n)
+#   Return the reversal of an integer, i.e., reverse(456) returns 654
+# end
+#
+# def is_palindrome(some_number)
+#   returns true if number is a palindrome, false if not
+# end
+#
+# Use the reverse_number method to implement is_palindrome. A number is a
+# palindrome if its reversal is the same as itself. Write a test program that
+# prompts the user to enter an integer and reports whether the integer is a
+# palindrome.
+#
+# Examples of palindromes:
+# 131
+# 22422
+# 12321
+# 66
+# 9990999
+#
+# HINT: it's much easier to reverse an array than a number or a string...
