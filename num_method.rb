45b2bfb80368962012672edc5f48ae87554cd40c
Robin Swenson-Healey <rjswenson@gmail.com>
2013-07-07 20:07:55 -0700

    perfect gen methods

--- num_method.rb
+++ num_method.rb
@@ -1,45 +1,48 @@
 
-def get_factors(num1)           #factors here
+def get_factors(num1)      #Grab the factors for a particular number     
   factors = [] 
-  for n in 1..num1
-    if num1%n == 0
+  for n in 1..num1        
+    if num1%n == 0        
       factors.push(n)
     end
   end
-  p factors.inspect
   return factors
 end
 
- def sum_of_array(array)
-  $sum = 0
+ def sum_of_array(array)   #Add all of the factors together
+  sum = 0
   for n in 0..array.size-1
-    $sum = $sum +array[n]
+    sum = sum +array[n]
   end
-return $sum
+  return sum
 end
 
-def perfect_check(num1)
-  if num1 == ($sum/2)
-    puts "#{num1} is a perfect number."
-    $sum = 0
-    $factors.clear
+
+def perfect_check(num1)   #Divide above number by two, and check against particular number
+  factors_arr = get_factors(num1)
+  sum_of_factors = sum_of_array(factors_arr)
+  if num1 == sum_of_factors/2
+    return true
   else
-    $sum = 0
-    $factors.clear
+    return false
   end
 end
+
 
 
 p "Let's find some perfect numbers."
 p "First, give me the low number in a range."
 input_low = gets.chomp.to_i
 p "Next, give a high number for the range."
-input_hight = gets.chomp.to_i
+input_high = gets.chomp.to_i
 
 
-for x in input_low..input high
-  get_factors(x)
-  sum_of_array($factors)
-  perfect_check(x)
+for x in input_low..input_high
+  is_perfect = perfect_check(x)
+  if is_perfect == true
+    p x 
+  end
 end
+
+
 
