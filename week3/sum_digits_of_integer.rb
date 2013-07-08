97142584c38155223ed0dcc76e0dd3c410bc25ad
Robin Swenson-Healey <rjswenson@gmail.com>
2013-07-08 12:11:15 -0700

    digit addition

--- week3/sum_digits_of_integer.rb
+++ week3/sum_digits_of_integer.rb
@@ -19,3 +19,39 @@
 #
 # Write a test program that prompts the user to enter an integer and displays
 # the sum of all its digits.
+
+
+#first, count the digits in the input.  For each digit, modulo
+#the input by 10^n where n is each number of digits.  ie.
+# if 234 has 3 digits, 234%10^1, 234%10^2, 234%
+#
+
+def sum_digits(n)    #Algebraeic Method
+  num_of_digits = n.to_s.length
+  sum = 0
+  n = n.to_i
+  for x in 1..num_of_digits
+    sum += ((n%(10**x))-(n%10**(x-1)))/(10**(x-1))
+  end
+  return sum
+end
+
+check = gets.chomp
+answer = sum_digits(check)
+p "The sum of digits for your number are: #{answer}"
+
+
+def sum_string(n)    #String Method
+  number = 0
+  num = n.split(//)
+  to_int = num.collect {|i| i.to_i}
+  string_add = to_int.each {|i| number = number + i}
+  return number
+end
+
+input = gets.chomp.to_s
+solution = sum_string(input)
+p "The sum of digits for your number are: #{solution}"
+
+
+  
