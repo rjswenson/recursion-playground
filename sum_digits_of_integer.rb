52cf578e6d88248962c5f2d3b80c0887055f8efb
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2013-07-07 20:26:23 -0700

    Adding digit-summing problem

---
+++ sum_digits_of_integer.rb
@@ -0,0 +1,21 @@
+################################
+### Sum the digits in an integer
+###
+#
+# Write a method that computes the sum of the digits in an integer.
+#
+# Use the following method signature:
+#
+# def sum_digits(n)
+#
+# end
+#
+# For example, sum_digits(234) returns 9 (2 + 3 + 4).
+#
+# (Hint: Use the % operator to extract digits, and the / operato to remove the
+# extracted digit. For instance, to extract 4 from 234, use 234 % 10 (= 4). To
+# remove 4 from 234, use 234 / 10 (= 23). Use a loop to repeatedly extract and
+# remove the digit until all the digits are extracted.
+#
+# Write a test program that prompts the user to enter an integer and displays
+# the sum of all its digits.
